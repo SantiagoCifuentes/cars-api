17c784e9258d025efcc9662599456112bcdddf5c
Santiago Cifuentes <santiago.ci9619@gmail.com>
2025-09-29 22:50:42 -0500

    Modelado de clases completado

---
+++ src/main/java/com/santiago/cars/model/Brands.java
@@ -0,0 +1,41 @@
+package com.santiago.cars.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "brands")
+public class Brands {
+
+    @Id
+    private Long id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String name;
+
+    public Brands() {
+    }
+
+    public Brands(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/santiago/cars/model/Cars.java
@@ -0,0 +1,87 @@
+package com.santiago.cars.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Cars {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 30, nullable = false)
+    private String carModel;
+
+    @Column(length = 100)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer mileage;
+
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id", nullable = false)
+    private Brands  brands;
+
+
+    public Cars(Long id, String carModel, String description, Integer price, Integer mileage) {
+        this.id = id;
+        this.carModel = carModel;
+        this.description = description;
+        this.price = price;
+        this.mileage = mileage;
+    }
+
+    public Cars (){
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+}
